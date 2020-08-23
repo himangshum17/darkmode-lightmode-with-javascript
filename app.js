342e1a50afef9e8bc82b421289c78668f90e842f
Himangshu Mondal <mailtoherock@gmail.com>
2020-08-23 17:22:18 +0530

    javascript functionality added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+
+let logo = document.getElementById("logo");
+
+document.querySelector("#dark-light-toggle").addEventListener("click", () => {
+  document.body.classList.toggle("darkmode");
+  if(logo.innerHTML === "Light Mode"){
+      logo.innerHTML = "Dark Mode"
+  }else{
+      logo.innerHTML = "Light Mode"
+  }
+});
